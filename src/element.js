81505102a241145b6295d2aba520dff776d8663d
leewang <lirenxn@gmail.com>
2020-04-08 18:02:05 +1000

    2: create Meact element

---
+++ src/element.js
@@ -0,0 +1,17 @@
+// <div id="container">
+//   <input value="foo" type="text">
+//   <a href="/bar"></a>
+//   <span></span>
+// </div>
+
+const element = {
+  type: "div",
+  props: {
+    id: "container",
+    children: [
+      { type: "input", props: { value: "foo", type: "text" } },
+      { type: "a", props: { href: "/bar" } },
+      { type: "span", props: {} },
+    ],
+  },
+};
