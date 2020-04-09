d2639bef46edcc3c775a64c8a90b83e9d04eed71
leewang <lirenxn@gmail.com>
2020-04-09 18:22:15 +1000

    5.1 createVNode

--- src/render.js
+++ src/render.js
@@ -1,5 +1,5 @@
-export function render(node, parentDom) {
-  const { type, props = {} } = node;
+export function render(element, parentDom) {
+  const { type, props = {} } = element;
 
   // create dom element
   const el =
@@ -22,7 +22,45 @@ export function render(node, parentDom) {
     }
   });
   // recurrsivly render children
-  (props.children || []).map((child) => render(child, el));
+  (props.children || []).forEach((child) => render(child, el));
   // attach Dom element to parent node
   parentDom.appendChild(el);
+}
+
+export function createVNode(element) {
+  const { type, props = {} } = element;
+  // create dom element
+  const dom =
+    type === "TEXT ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(type);
+
+  // map element props to dom properties
+  Object.keys(props).forEach((key) => {
+    if (key === "children") {
+      return;
+    }
+    // bind event listeners
+    if (key.startsWith("on")) {
+      const eventName = key.toLowerCase().substring(2);
+      dom.addEventListener(eventName, props[key]);
+    } else {
+      // map normal DOM properties
+      dom[key] = props[key];
+    }
+  });
+
+  // recurrsivly initiate children
+  const childInstances = (props.children || []).map((child) =>
+    createVNode(child)
+  );
+  childInstances.forEach((child) => {
+    dom.appendChild(child.dom);
+  });
+
+  return {
+    dom,
+    element,
+    childInstances,
+  };
 }
