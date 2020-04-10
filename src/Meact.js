784c77b5f3d5c2bae5226c423ca710cef18685dd
leewang <lirenxn@gmail.com>
2020-04-10 15:43:06 +1000

    6.1 make Class Component base

--- src/Meact.js
+++ src/Meact.js
@@ -1,3 +1,21 @@
+import { updateClassNode } from "./render";
+class Component {
+  constructor(props) {
+    this.props = props;
+    this.state = this.state || {};
+  }
+
+  setState(partialState) {
+    this.state = {
+      ...this.state,
+      ...partialState,
+    };
+    updateClassNode(this.__internalVNode);
+  }
+
+  render() {}
+}
+
 const createTextElement = (text) => ({
   type: "TEXT ELEMENT",
   props: { nodeValue: text },
@@ -22,6 +40,7 @@ const createElement = (type, nullableProps, ...rawChildren) => {
 const Meact = {
   createElement,
   createTextElement,
+  Component,
 };
 
 export default Meact;
